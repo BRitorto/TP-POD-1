40e0aaf10751317bc515428de739571064599d8b
Esteban Kramer <estebankramer@gmail.com>
2019-09-15 23:14:46 -0300

    getParty on clientInterface

--- api/src/main/java/ar/edu/itba/pod/ClientInterface.java
+++ api/src/main/java/ar/edu/itba/pod/ClientInterface.java
@@ -1,4 +1,5 @@
 package ar.edu.itba.pod;
+import ar.edu.itba.pod.model.Party;
 import ar.edu.itba.pod.model.Vote;
 
 import java.io.Serializable;
@@ -9,7 +10,9 @@ public interface ClientInterface extends Remote, Serializable {
 
     void ping() throws RemoteException;
 
-    void notifyChanges(Vote vote) throws RemoteException;
+    void notifyChanges(Party party, Long totalVotes) throws RemoteException;
 
     void setId(long id) throws RemoteException;
+
+    Party getParty() throws RemoteException;
 }
